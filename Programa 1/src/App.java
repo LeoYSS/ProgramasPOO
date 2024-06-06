bdd572c30e85f2f984d1ce658bee7165cadd25b2
Leonardo Sanchez <sasleonardo0309@gmail.com>
2024-06-05 21:42:32 -0600

    Mejoras en el programa

--- Programa 1/src/App.java
+++ Programa 1/src/App.java
@@ -1,33 +1,29 @@
 import java.util.Scanner;
 
 public class App {
-    public class Inicio {
-        String name;
-        // metodos
-        public static void seleccion(){
-            String[] pokemon = new String[4];
-            pokemon[0]="Charmander";
-            pokemon[1]="Bulbasur";
-            pokemon[2]="Squirtle";
-            pokemon[3]="Pikachu";
-            System.out.println("Los pokemones son: ");
-            for(int i = 0; i<=pokemon.length; i++){
-                System.out.println(pokemon[i]);
-            }
-        }
-        // Constructor
-        
-        
-    }
+
     public static void main(String[]args){
         Scanner teclado = new Scanner(System.in);
+        String[] pokemon = {"Pikachu","Charmander","Squirtle","Bulbasur"};
+            
         System.out.println("BIENVENIDO AL MUNDO POKEMON");
         System.out.println("Vamos a inicar la aventura seleccionando uno de los 4 pokemones");
-        // Llama el metodo de la clase padre
-        Inicio.seleccion();
-        System.out.println("Seleccione uno");
-        String us1 = teclado.nextLine();
+        System.out.println("Los pokemones son: ");
+        for(int i=0; i< pokemon.length; i++){
+            System.out.println("-> "+ pokemon[i]+ " ");
+        }
+        System.out.println("SELECCIONE UNO");
+        String opcion = teclado.nextLine();
+        Interaccion usuario = new Interaccion(opcion);
+    }
+    
+}
 
+class Interaccion{
+    protected String name;
+    // constructor
+    public Interaccion(String name){
+        this.name = name;
     }
     
 }
