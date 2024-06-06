4183cb081ed1ce2e30e08db41381747eb30874c7
Leonardo Sanchez <sasleonardo0309@gmail.com>
2024-06-05 22:38:19 -0600

    Version standar de programa pokemon

--- Programa 1/src/App.java
+++ Programa 1/src/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[]args){
         Scanner teclado = new Scanner(System.in);
-        String[] pokemon = {"Pikachu","Charmander","Squirtle","Bulbasur"};
+        String[] pokemon = {"Pikachu (1)","Charmander (2)","Squirtle (3)","Bulbasur (4)"};
             
         System.out.println("BIENVENIDO AL MUNDO POKEMON");
         System.out.println("Vamos a inicar la aventura seleccionando uno de los 4 pokemones");
@@ -13,17 +13,64 @@ public class App {
             System.out.println("-> "+ pokemon[i]+ " ");
         }
         System.out.println("SELECCIONE UNO");
-        String opcion = teclado.nextLine();
-        Interaccion usuario = new Interaccion(opcion);
+        int opcion = teclado.nextInt();
+        String name;
+        // System.out.println("Mostrando caracteristicas...");
+        switch (opcion) {
+                case 1:
+                        // this.opcion = name;
+                break;
+                case 2:
+                    // this.opcion = name;
+                break;
+                case 3:
+                    name = "Squirtle";
+                    PokemonIn usuario = new PokemonIn(name);
+                    
+                break;
+                case 4:
+                    //  name = ;
+                break;
+            default:
+                System.out.println("ERROR");
+                break;
+        }
+
     }
     
 }
-
-class Interaccion{
+// clase padre
+class PokemonIn {
     protected String name;
-    // constructor
-    public Interaccion(String name){
+    public PokemonIn(String name) {
         this.name = name;
     }
     
+    
+}
+class Acciones extends PokemonIn{
+    public Acciones(String name) {
+        super(name);
+    }
+
+}
+class Squirtle{
+    // atributos
+    protected String tipo = "Agua";
+    protected String Pokemon = "Tortuga";
+    protected String color = "Azul";
+    protected String evolucion = "Wartortle y Blastoise";
+    // metodos
+    public void PlacajeTackle(){
+        System.out.println("Placaje Tackle = baja de un 10 a 20 % de vida del rival");
+    }
+    public void LatigoTailwhip(){
+        System.out.println("Latigo Tailwhip = baja la resistencia del rival en un 10 al 15%");
+    }
+    public void PistolaDeAgua(){
+        System.out.println("Pistola De Agua = baja un 10% de vida del rival y disminuye ataques de fuego");
+    }
+    public void RefugioWithdraw(){
+        System.out.println("Refugio Withdraw = aumenta la resistencia en un 15%");
+    }
 }
