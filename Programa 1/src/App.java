06f8f4ee357870b21d36f51a5ea03ab8c2dcbeeb
NopsssA <garcialunaangelgabriel042@gmail.com>
2024-06-07 08:19:54 -0600

    Update App.java

--- Programa 1/src/App.java
+++ Programa 1/src/App.java
@@ -5,6 +5,7 @@ public class App {
         Pokemon PokemonInicio [] = new Pokemon[3];
         PokemonInicio[0] = new Squirtle("SQUIRTLE","Agua", "Tortuga", "Azul", "Wartortle y Blastoise");
         PokemonInicio[1] = new Bulbasur("BULBASUR","Planta/Veneno", "Semilla", "Verde con manchas oscuras", "Ivysaur y Venusaur");
+        PokemonInicio[2]=new Pikachu("PIKACHU","Electrico","Rata","Amarillo","Raichu");
         System.out.println("BIENVENIDO AL MUNDO POKEMON");
         System.out.println("Vamos a inicar la aventura seleccionando uno de los 4 pokemones");
         System.out.println("Los pokemones son: "+"\n");
